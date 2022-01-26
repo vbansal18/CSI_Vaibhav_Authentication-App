9b6524b32eecbb451ef14d7136bc53e46accac37
vbansal18 <vaibhavbansal369@gmail.com>
2022-01-26 18:16:35 +0530

    second commit

--- app/src/main/java/com/example/signup/MainActivity.kt
+++ app/src/main/java/com/example/signup/MainActivity.kt
@@ -12,13 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val options = PhoneAuthOptions.newBuilder(auth)
-            .setPhoneNumber(phoneNumber)       // Phone number to verify
-            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-            .setActivity(this)                 // Activity (for callback binding)
-            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
-            .build()
-        PhoneAuthProvider.verifyPhoneNumber(options)
 
         val branch: Spinner = findViewById(R.id.branch)
         ArrayAdapter.createFromResource(
